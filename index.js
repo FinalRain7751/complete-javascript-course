8b90437a79d1ae26899d74049d8d1e24ce40446a
Souransu Sen <souransu2sen@gmail.com>
2023-05-11 02:40:37 +0530

    upto let,const and var

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Hello, world!");
+const country = "India";
+const continent = "Asia";
+let populationInMillions = 1400;
+
+console.log(country, continent, populationInMillions);
+
+const isIsland = false;
+let language;
+language = "Bengali";
+// country = "China"; Assignment to const variable cannot be done
+
+console.log(
+  typeof isIsland,
+  typeof populationInMillions,
+  typeof country,
+  typeof language
+);
+
+let halfPopulation = populationInMillions / 2;
+
+populationInMillions++;
+
+console.log(populationInMillions);
+let populationOfFinland = 6;
+
+if (populationInMillions > populationOfFinland) {
+  console.log("yes");
+}
+
+let averagePopulation = 33;
+if (populationInMillions > averagePopulation) {
+  console.log("yes");
+}
+
+let description = `${country} is in ${continent}, and its ${populationInMillions} million people speak ${language}.`;
+
+console.log(description);
