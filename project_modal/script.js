e9c1cbdd3ca3fcf9f02535c15ab36aaefa5056ca
Souransu Sen <souransu2sen@gmail.com>
2023-05-20 01:12:53 +0530

    project_modal done

--- project_modal/script.js
+++ project_modal/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+// Shows Modal
+const showModalButtons = document.querySelectorAll('.show-modal');
+for (let button of showModalButtons) {
+  button.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+// Closes Modal
+const closeModalButton = document.querySelector('.close-modal');
+closeModalButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
+
+function closeModal() {
+  if (!modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+}
